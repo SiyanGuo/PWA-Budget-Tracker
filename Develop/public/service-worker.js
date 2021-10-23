c0af916c84ea4719f37049a359ca4955893b04f3
Siyan Guo <siyang@Hey-There.local>
2021-10-23 18:43:12 -0400

    register service worker

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,10 @@
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/style.css",
+    "./js/index.js",
+    "./js/idb.js",
+  ];
